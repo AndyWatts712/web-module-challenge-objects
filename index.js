3a3a96308515b2298f31edffc10f3a972f3315b2
megawatts68 <andrew.f.watts@gmail.com>
2020-06-11 05:50:30 +0700

    Task 3 complete.

--- index.js
+++ index.js
@@ -60,6 +60,11 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 
 /* Task 4: Console.log just Julius' feedback */
 
+for (i = 0; i < reviews.length; i++) {
+  if (reviews[i].name === "Julius") {
+    console.log(reviews[i].feedback);
+  }
+}
 
 /* Task 5: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
 
